dce98c7d91f155fe9f94f6dbbdeed2ebd90945e5
Shaina Couch <shainacouch21@gmail.com>
2023-02-10 10:56:27 -0600

    old calculator app

---
+++ main.js
@@ -0,0 +1,29 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+
+const subtract =(firstNumber, secondNumber) => {
+    const difference = firstNumber + secondNumber
+    return difference
+}
+
+const divide = (firstNumber, secondNumber) => {
+    const quotient = firstNumber / secondNumber
+    return quotient
+}
+
+const multiply = (firstNumber, secondNumber) => {
+    const product = firstNumber * secondNumber
+    return product
+}
+
+const square = (number) => {
+    const squared = number * number
+    return squared
+}
+
+
+
+let answer = add(60,30)
+console.log(answer)
